7885a66bdb5e01a322c11497e040b235f9769266
Tonow-c2c <thomas.nowicki@camptocamp.com>
2017-09-26 15:07:22 +0200

    first commit (dont work)

---
+++ my_module/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Library Books",
+    'summary': "Manage your books",
+    'depends': ['base'],
+    'data': ['views/library_book.xml'],
+}
+
+'''
+{
+    'name': "Title",
+    'summary': "Short subtitle phrase",
+    'description': """Long description""",
+    'author': "Your name",
+    'license': "AGPL-3",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '9.0.1.0.0',
+    'depends': ['base'],
+    'data': ['views.xml'],
+    'demo': ['demo.xml'],
+}
+'''
